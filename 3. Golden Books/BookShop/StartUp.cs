9b3e911c1b89f69b07896c510988a12a8d7b9627
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2023-11-16 00:29:08 +0200

    3. Golden Books

---
+++ 3. Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            // DbInitializer.ResetDatabase(db);
+
+            // string input = Console.ReadLine();
+
+            Console.WriteLine(GetGoldenBooks(db));
+        }
+
+        // Ex. 2
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            if (!Enum.TryParse<AgeRestriction>(command, true, out var ageRestriction))
+            {
+                return $"{command} is not a valid age restriction";
+            }
+
+            var books = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => new
+                {
+                    b.Title
+                })
+                .OrderBy(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books.Select(b => b.Title));
+        }
+
+        // Ex. 3
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.EditionType == EditionType.Gold &&
+                            b.Copies < 5000)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books.Select(b=>b.Title));
+        }
+    }
+}
+
+
