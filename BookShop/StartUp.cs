4f0fb8dd49233bd8623e8a25f231b1165903526d
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2023-11-15 20:37:51 +0200

    BookShop System. 2. Age Restriction

---
+++ BookShop/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            // DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine(GetBooksByAgeRestriction(db, input));
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            if (!Enum.TryParse<AgeRestriction>(command, true, out var ageRestriction))
+            {
+                return $"{command} is not a valid are restriction";
+            }
+
+            var books = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => new
+                {
+                    b.Title
+                })
+                .OrderBy(b => b.Title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, books.Select(b => b.Title));
+        }
+    }
+}
+
+
