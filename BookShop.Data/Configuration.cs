ee7851736529e9b3882f7f02fd9a30990b0fcf68
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2023-11-16 00:32:16 +0200

    Delete BookShop.Data directory

--- BookShop.Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookShop.Data
-{
-    internal class Configuration
-    {
-        internal static string ConnectionString
-            => "Server=DESKTOP-O3GKITC;Database=BookShop;Integrated Security=True;";
-    }
-}
