412e160705239c4b7c7625e25bd35a92d372b801
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2023-11-16 15:10:09 +0200

    Add files via upload

---
+++ 5. Not Released In/BookShop.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=DESKTOP-O3GKITC;Database=BookShop;Integrated Security=True;";
+    }
+}
